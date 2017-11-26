afd4a67640b6492e4de73558298a0e4bf8fba1d0
itchyny <itchyny@hatena.ne.jp>
2017-11-26 22:20:46 +0900

    fix tests for key manager

--- core/key_test.go
+++ core/key_test.go
@@ -6,12 +6,12 @@ func TestKeyManagerPress(t *testing.T) {
 	km := NewKeyManager()
 	km.Register(CursorUp, "k")
 	e := km.Press("k")
-	if e != CursorUp {
-		t.Errorf("pressing k should emit CursorUp but got: %d", e)
+	if e.Type != CursorUp {
+		t.Errorf("pressing k should emit CursorUp but got: %d", e.Type)
 	}
 	e = km.Press("j")
-	if e != Nop {
-		t.Errorf("pressing j should be nop but got: %d", e)
+	if e.Type != Nop {
+		t.Errorf("pressing j should be nop but got: %d", e.Type)
 	}
 }
 
@@ -21,19 +21,19 @@ func TestKeyManagerPressMulti(t *testing.T) {
 	km.Register(CursorDown, "k", "j", "j")
 	km.Register(CursorDown, "j", "k", "k")
 	e := km.Press("k")
-	if e != Nop {
-		t.Errorf("pressing k should be nop but got: %d", e)
+	if e.Type != Nop {
+		t.Errorf("pressing k should be nop but got: %d", e.Type)
 	}
 	e = km.Press("k")
-	if e != Nop {
-		t.Errorf("pressing k twice should be nop but got: %d", e)
+	if e.Type != Nop {
+		t.Errorf("pressing k twice should be nop but got: %d", e.Type)
 	}
 	e = km.Press("k")
-	if e != Nop {
-		t.Errorf("pressing k three times should be nop but got: %d", e)
+	if e.Type != Nop {
+		t.Errorf("pressing k three times should be nop but got: %d", e.Type)
 	}
 	e = km.Press("j")
-	if e != CursorUp {
-		t.Errorf("pressing kkj should emit CursorUp but got: %d", e)
+	if e.Type != CursorUp {
+		t.Errorf("pressing kkj should emit CursorUp but got: %d", e.Type)
 	}
 }
