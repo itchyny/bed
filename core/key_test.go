ffdb9db20a9e7347a516bd5f7c118da7255b7d58
itchyny <itchyny@hatena.ne.jp>
2017-11-26 22:23:15 +0900

    add tests for count parsing in key manager

--- core/key_test.go
+++ core/key_test.go
@@ -37,3 +37,31 @@ func TestKeyManagerPressMulti(t *testing.T) {
 		t.Errorf("pressing kkj should emit CursorUp but got: %d", e.Type)
 	}
 }
+
+func TestKeyManagerPressCount(t *testing.T) {
+	km := NewKeyManager()
+	km.Register(CursorUp, "k", "j")
+	e := km.Press("k")
+	if e.Type != Nop {
+		t.Errorf("pressing k should be nop but got: %d", e.Type)
+	}
+	e = km.Press("3")
+	if e.Type != Nop {
+		t.Errorf("pressing 3 should be nop but got: %d", e.Type)
+	}
+	e = km.Press("7")
+	if e.Type != Nop {
+		t.Errorf("pressing 7 should be nop but got: %d", e.Type)
+	}
+	e = km.Press("k")
+	if e.Type != Nop {
+		t.Errorf("pressing k should be nop but got: %d", e.Type)
+	}
+	e = km.Press("j")
+	if e.Type != CursorUp {
+		t.Errorf("pressing 37kj should emit CursorUp but got: %d", e.Type)
+	}
+	if e.Count != 37 {
+		t.Errorf("pressing 37kj should emit CursorUp with count 37 but got: %d", e.Count)
+	}
+}
