d15d5eb25cf2d848e03ef20065caef17d31cbaa4
itchyny <itchyny@hatena.ne.jp>
2018-03-17 23:54:40 +0900

    add comments for exported methods of Layout structs

--- common/layout.go
+++ common/layout.go
@@ -2,6 +2,7 @@ package common
 
 import "github.com/itchyny/bed/util"
 
+// Layout represents the window layout.
 type Layout interface {
 	isLayout()
 	Indices() []int
@@ -14,21 +15,25 @@ type Layout interface {
 	Close() Layout
 }
 
+// LayoutWindow holds the window index and it is active or not.
 type LayoutWindow struct {
 	Index  int
 	Active bool
 }
 
+// NewLayout creates a new Layout from a window index.
 func NewLayout(index int) Layout {
 	return LayoutWindow{Index: index, Active: true}
 }
 
-func (_ LayoutWindow) isLayout() {}
+func (l LayoutWindow) isLayout() {}
 
+// Indices returns all the window indeces.
 func (l LayoutWindow) Indices() []int {
 	return []int{l.Index}
 }
 
+// SplitTop splits the layout and opens a new window to the top.
 func (l LayoutWindow) SplitTop(index int) Layout {
 	if !l.Active {
 		return l
@@ -39,6 +44,7 @@ func (l LayoutWindow) SplitTop(index int) Layout {
 	}
 }
 
+// SplitBottom splits the layout and opens a new window to the bottom.
 func (l LayoutWindow) SplitBottom(index int) Layout {
 	if !l.Active {
 		return l
@@ -49,6 +55,7 @@ func (l LayoutWindow) SplitBottom(index int) Layout {
 	}
 }
 
+// SplitLeft splits the layout and opens a new window to the left.
 func (l LayoutWindow) SplitLeft(index int) Layout {
 	if !l.Active {
 		return l
@@ -59,6 +66,7 @@ func (l LayoutWindow) SplitLeft(index int) Layout {
 	}
 }
 
+// SplitRight splits the layout and opens a new window to the right.
 func (l LayoutWindow) SplitRight(index int) Layout {
 	if !l.Active {
 		return l
@@ -69,15 +77,18 @@ func (l LayoutWindow) SplitRight(index int) Layout {
 	}
 }
 
+// Count returns the width and height counts.
 func (l LayoutWindow) Count() (int, int) {
 	return 1, 1
 }
 
+// Activate the first layout.
 func (l LayoutWindow) Activate() Layout {
 	l.Active = true
 	return l
 }
 
+// Close the active layout.
 func (l LayoutWindow) Close() Layout {
 	if l.Active {
 		panic("Active LayoutWindow should not be closed")
@@ -85,17 +96,20 @@ func (l LayoutWindow) Close() Layout {
 	return l
 }
 
+// LayoutHorizontal holds two layout horizontally.
 type LayoutHorizontal struct {
 	Top    Layout
 	Bottom Layout
 }
 
-func (_ LayoutHorizontal) isLayout() {}
+func (l LayoutHorizontal) isLayout() {}
 
+// Indices returns all the window indeces.
 func (l LayoutHorizontal) Indices() []int {
 	return append(l.Top.Indices(), l.Bottom.Indices()...)
 }
 
+// SplitTop splits the layout and opens a new window to the top.
 func (l LayoutHorizontal) SplitTop(index int) Layout {
 	return LayoutHorizontal{
 		Top:    l.Top.SplitTop(index),
@@ -103,6 +117,7 @@ func (l LayoutHorizontal) SplitTop(index int) Layout {
 	}
 }
 
+// SplitBottom splits the layout and opens a new window to the bottom.
 func (l LayoutHorizontal) SplitBottom(index int) Layout {
 	return LayoutHorizontal{
 		Top:    l.Top.SplitBottom(index),
@@ -110,6 +125,7 @@ func (l LayoutHorizontal) SplitBottom(index int) Layout {
 	}
 }
 
+// SplitLeft splits the layout and opens a new window to the left.
 func (l LayoutHorizontal) SplitLeft(index int) Layout {
 	return LayoutHorizontal{
 		Top:    l.Top.SplitLeft(index),
@@ -117,6 +133,7 @@ func (l LayoutHorizontal) SplitLeft(index int) Layout {
 	}
 }
 
+// SplitRight splits the layout and opens a new window to the right.
 func (l LayoutHorizontal) SplitRight(index int) Layout {
 	return LayoutHorizontal{
 		Top:    l.Top.SplitRight(index),
@@ -124,12 +141,14 @@ func (l LayoutHorizontal) SplitRight(index int) Layout {
 	}
 }
 
+// Count returns the width and height counts.
 func (l LayoutHorizontal) Count() (int, int) {
 	w1, h1 := l.Top.Count()
 	w2, h2 := l.Bottom.Count()
 	return util.MaxInt(w1, w2), h1 + h2
 }
 
+// Activate the first layout.
 func (l LayoutHorizontal) Activate() Layout {
 	return LayoutHorizontal{
 		Top:    l.Top.Activate(),
@@ -137,6 +156,7 @@ func (l LayoutHorizontal) Activate() Layout {
 	}
 }
 
+// Close the active layout.
 func (l LayoutHorizontal) Close() Layout {
 	switch m := l.Top.(type) {
 	case LayoutWindow:
@@ -156,17 +176,20 @@ func (l LayoutHorizontal) Close() Layout {
 	}
 }
 
+// LayoutVertical holds two layout vertically.
 type LayoutVertical struct {
 	Left  Layout
 	Right Layout
 }
 
-func (_ LayoutVertical) isLayout() {}
+func (l LayoutVertical) isLayout() {}
 
+// Indices returns all the window indeces.
 func (l LayoutVertical) Indices() []int {
 	return append(l.Left.Indices(), l.Right.Indices()...)
 }
 
+// SplitTop splits the layout and opens a new window to the top.
 func (l LayoutVertical) SplitTop(index int) Layout {
 	return LayoutVertical{
 		Left:  l.Left.SplitTop(index),
@@ -174,6 +197,7 @@ func (l LayoutVertical) SplitTop(index int) Layout {
 	}
 }
 
+// SplitBottom splits the layout and opens a new window to the bottom.
 func (l LayoutVertical) SplitBottom(index int) Layout {
 	return LayoutVertical{
 		Left:  l.Left.SplitBottom(index),
@@ -181,6 +205,7 @@ func (l LayoutVertical) SplitBottom(index int) Layout {
 	}
 }
 
+// SplitLeft splits the layout and opens a new window to the left.
 func (l LayoutVertical) SplitLeft(index int) Layout {
 	return LayoutVertical{
 		Left:  l.Left.SplitLeft(index),
@@ -188,6 +213,7 @@ func (l LayoutVertical) SplitLeft(index int) Layout {
 	}
 }
 
+// SplitRight splits the layout and opens a new window to the right.
 func (l LayoutVertical) SplitRight(index int) Layout {
 	return LayoutVertical{
 		Left:  l.Left.SplitRight(index),
@@ -195,12 +221,14 @@ func (l LayoutVertical) SplitRight(index int) Layout {
 	}
 }
 
+// Count returns the width and height counts.
 func (l LayoutVertical) Count() (int, int) {
 	w1, h1 := l.Left.Count()
 	w2, h2 := l.Right.Count()
 	return w1 + w2, util.MaxInt(h1, h2)
 }
 
+// Activate the first layout.
 func (l LayoutVertical) Activate() Layout {
 	return LayoutVertical{
 		Left:  l.Left.Activate(),
@@ -208,6 +236,7 @@ func (l LayoutVertical) Activate() Layout {
 	}
 }
 
+// Close the active layout.
 func (l LayoutVertical) Close() Layout {
 	switch m := l.Left.(type) {
 	case LayoutWindow:
