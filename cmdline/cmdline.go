804b404d238551ae25bd335f06f5d34a140deccc
itchyny <itchyny@hatena.ne.jp>
2018-03-22 22:14:52 +0900

    implement completor struct and move codes for cmdline completion

--- cmdline/cmdline.go
+++ cmdline/cmdline.go
@@ -1,13 +1,7 @@
 package cmdline
 
 import (
-	"os"
-	"path/filepath"
-	"sort"
-	"strings"
 	"unicode"
-
-	"github.com/mitchellh/go-homedir"
 
 	. "github.com/itchyny/bed/common"
 	"github.com/itchyny/bed/util"
@@ -17,7 +11,7 @@ import (
 type Cmdline struct {
 	cmdline           []rune
 	cursor            int
-	completionTarget  string
+	completor         *completor
 	completionResults []string
 	completionIndex   int
 	eventCh           chan<- Event
@@ -27,7 +21,7 @@ type Cmdline struct {
 
 // NewCmdline creates a new Cmdline.
 func NewCmdline() *Cmdline {
-	return &Cmdline{}
+	return &Cmdline{completor: newCompletor()}
 }
 
 // Init initializes the Cmdline.
@@ -76,8 +70,7 @@ func (c *Cmdline) Run() {
 		default:
 			continue
 		}
-		c.completionResults = nil
-		c.completionIndex = 0
+		c.completor.clear()
 		c.redrawCh <- struct{}{}
 	}
 }
@@ -152,173 +145,11 @@ func (c *Cmdline) insert(ch rune) {
 func (c *Cmdline) complete(forward bool) {
 	cmd, args, err := parse(c.cmdline)
 	if err != nil {
-		c.completionResults = nil
-		c.completionIndex = 0
+		c.completor.clear()
 		return
 	}
-	if cmd.eventType != EventEdit && cmd.eventType != EventNew && cmd.eventType != EventVnew {
-		c.completionResults = nil
-		c.completionIndex = 0
-		return
-	}
-	cmdName := strings.Fields(string(c.cmdline))[0] // todo: parse should return argument position
-	if len(c.completionResults) > 0 {
-		if forward {
-			c.completionIndex = (c.completionIndex+2)%(len(c.completionResults)+1) - 1
-		} else {
-			c.completionIndex = (c.completionIndex+len(c.completionResults)+1)%(len(c.completionResults)+1) - 1
-		}
-		if c.completionIndex < 0 {
-			c.cmdline = []rune(c.completionTarget)
-		} else {
-			c.cmdline = []rune(cmdName + " " + c.completionResults[c.completionIndex])
-		}
-		c.cursor = len(c.cmdline)
-		return
-	}
-	c.completionTarget = string(c.cmdline)
-	c.completionIndex = 0
-	if len(args) == 0 {
-		c.completionResults = listFileNames("")
-	} else if len(args) == 1 {
-		c.completionResults = listFileNames(args[0])
-	}
-	if len(c.completionResults) == 1 {
-		c.cmdline = []rune(cmdName + " " + c.completionResults[0])
-		c.cursor = len(c.cmdline)
-		c.completionResults = nil
-	} else if len(c.completionResults) > 1 {
-		if forward {
-			c.cmdline = []rune(cmdName + " " + c.completionResults[0])
-			c.completionIndex = 0
-			c.cursor = len(c.cmdline)
-		} else {
-			c.cmdline = []rune(cmdName + " " + c.completionResults[len(c.completionResults)-1])
-			c.completionIndex = len(c.completionResults) - 1
-			c.cursor = len(c.cmdline)
-		}
-		c.completionTarget = cmdName + " " + samePrefix(c.completionResults)
-	}
-}
-
-func listFileNames(prefix string) []string {
-	var targets []string
-	separator := string(filepath.Separator)
-	if prefix == "" {
-		f, err := os.Open(".")
-		if err != nil {
-			return nil
-		}
-		defer f.Close()
-		fileInfos, err := f.Readdir(100)
-		if err != nil {
-			return nil
-		}
-		for _, fileInfo := range fileInfos {
-			name := fileInfo.Name()
-			if fileInfo.IsDir() {
-				name += separator
-			}
-			targets = append(targets, name)
-		}
-	} else {
-		path, err := homedir.Expand(prefix)
-		if err != nil {
-			return nil
-		}
-		homeDir, err := homedir.Dir()
-		if err != nil {
-			return nil
-		}
-		if len(prefix) > 1 && strings.HasSuffix(prefix, separator) ||
-			prefix[0] == '~' && strings.HasSuffix(prefix, "/.") {
-			path += separator
-		}
-		if !strings.HasSuffix(prefix, "/") && !strings.HasSuffix(prefix, ".") {
-			stat, err := os.Stat(path)
-			if err == nil && stat.IsDir() {
-				return []string{prefix + "/"}
-			}
-		}
-		dir, base := filepath.Dir(path), filepath.Base(path)
-		if strings.HasSuffix(path, separator) {
-			if strings.HasSuffix(prefix, "/.") {
-				base = "."
-			} else {
-				base = ""
-			}
-		}
-		f, err := os.Open(dir)
-		if err != nil {
-			return nil
-		}
-		defer f.Close()
-		fileInfos, err := f.Readdir(300)
-		if err != nil {
-			return nil
-		}
-		lowerBase := strings.ToLower(base)
-		for _, fileInfo := range fileInfos {
-			name := fileInfo.Name()
-			if base != separator && !strings.HasPrefix(strings.ToLower(name), lowerBase) {
-				continue
-			}
-			name = filepath.Join(dir, name)
-			if prefix[0] == '~' {
-				name = filepath.Join("~", strings.TrimLeft(name, homeDir))
-			}
-			if fileInfo.IsDir() {
-				name += separator
-			}
-			targets = append(targets, name)
-		}
-	}
-	sortFilePaths(targets)
-	return targets
-}
-
-func sortFilePaths(paths []string) {
-	sort.SliceStable(paths, func(i, j int) bool {
-		return filePathSorter(paths[i]) < filePathSorter(paths[j])
-	})
-}
-
-func filePathSorter(path string) string {
-	var ret string
-	if path[len(path)-1] == filepath.Separator {
-		ret += "1"
-	} else {
-		ret += "0"
-	}
-	if strings.HasPrefix(filepath.Base(path), ".") {
-		ret += "1"
-	} else {
-		ret += "0"
-	}
-	return ret + path
-}
-
-func samePrefix(results []string) string {
-	var xs string
-	for i, ys := range results {
-		if i == 0 {
-			xs = ys
-		} else {
-			yss := []rune(ys)
-			for j, x := range xs {
-				if j < len(yss) {
-					if x != yss[j] {
-						xs = string(yss[:j])
-						break
-					}
-				} else {
-					xs = string(yss)
-					break
-				}
-			}
-		}
-	}
-	return xs
+	c.cmdline = []rune(c.completor.complete(string(c.cmdline), cmd, args, forward))
+	c.cursor = len(c.cmdline)
 }
 
 func (c *Cmdline) execute() {
@@ -334,5 +165,5 @@ func (c *Cmdline) execute() {
 
 // Get returns the current state of cmdline.
 func (c *Cmdline) Get() ([]rune, int, []string, int) {
-	return c.cmdline, c.cursor, c.completionResults, c.completionIndex
+	return c.cmdline, c.cursor, c.completor.results, c.completor.index
 }
---
+++ cmdline/completor.go
@@ -0,0 +1,190 @@
+package cmdline
+
+import (
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/mitchellh/go-homedir"
+
+	. "github.com/itchyny/bed/common"
+)
+
+type completor struct {
+	target  string
+	results []string
+	index   int
+}
+
+func newCompletor() *completor {
+	return &completor{}
+}
+
+func (c *completor) clear() {
+	c.target = ""
+	c.results = nil
+	c.index = 0
+}
+
+func (c *completor) complete(cmdline string, cmd command, args []string, forward bool) string {
+	if cmd.eventType != EventEdit && cmd.eventType != EventNew && cmd.eventType != EventVnew {
+		c.results = nil
+		c.index = 0
+		return cmdline
+	}
+	cmdName := strings.Fields(cmdline)[0] // todo: parse should return argument position
+	if len(c.results) > 0 {
+		if forward {
+			c.index = (c.index+2)%(len(c.results)+1) - 1
+		} else {
+			c.index = (c.index+len(c.results)+1)%(len(c.results)+1) - 1
+		}
+		if c.index < 0 {
+			return c.target
+		}
+		return cmdName + " " + c.results[c.index]
+	}
+	c.target = cmdline
+	c.index = 0
+	if len(args) == 0 {
+		c.results = listFileNames("")
+	} else if len(args) == 1 {
+		c.results = listFileNames(args[0])
+	}
+	if len(c.results) == 1 {
+		cmdline := cmdName + " " + c.results[0]
+		c.results = nil
+		return cmdline
+	}
+	if len(c.results) > 1 {
+		c.target = cmdName + " " + samePrefix(c.results)
+		if forward {
+			c.index = 0
+			return cmdName + " " + c.results[0]
+		}
+		c.index = len(c.results) - 1
+		return cmdName + " " + c.results[len(c.results)-1]
+	}
+	return cmdline
+}
+
+func listFileNames(prefix string) []string {
+	var targets []string
+	separator := string(filepath.Separator)
+	if prefix == "" {
+		f, err := os.Open(".")
+		if err != nil {
+			return nil
+		}
+		defer f.Close()
+		fileInfos, err := f.Readdir(100)
+		if err != nil {
+			return nil
+		}
+		for _, fileInfo := range fileInfos {
+			name := fileInfo.Name()
+			if fileInfo.IsDir() {
+				name += separator
+			}
+			targets = append(targets, name)
+		}
+	} else {
+		path, err := homedir.Expand(prefix)
+		if err != nil {
+			return nil
+		}
+		homeDir, err := homedir.Dir()
+		if err != nil {
+			return nil
+		}
+		if len(prefix) > 1 && strings.HasSuffix(prefix, separator) ||
+			prefix[0] == '~' && strings.HasSuffix(prefix, "/.") {
+			path += separator
+		}
+		if !strings.HasSuffix(prefix, "/") && !strings.HasSuffix(prefix, ".") {
+			stat, err := os.Stat(path)
+			if err == nil && stat.IsDir() {
+				return []string{prefix + "/"}
+			}
+		}
+		dir, base := filepath.Dir(path), filepath.Base(path)
+		if strings.HasSuffix(path, separator) {
+			if strings.HasSuffix(prefix, "/.") {
+				base = "."
+			} else {
+				base = ""
+			}
+		}
+		f, err := os.Open(dir)
+		if err != nil {
+			return nil
+		}
+		defer f.Close()
+		fileInfos, err := f.Readdir(300)
+		if err != nil {
+			return nil
+		}
+		lowerBase := strings.ToLower(base)
+		for _, fileInfo := range fileInfos {
+			name := fileInfo.Name()
+			if base != separator && !strings.HasPrefix(strings.ToLower(name), lowerBase) {
+				continue
+			}
+			name = filepath.Join(dir, name)
+			if prefix[0] == '~' {
+				name = filepath.Join("~", strings.TrimLeft(name, homeDir))
+			}
+			if fileInfo.IsDir() {
+				name += separator
+			}
+			targets = append(targets, name)
+		}
+	}
+	sortFilePaths(targets)
+	return targets
+}
+
+func sortFilePaths(paths []string) {
+	sort.SliceStable(paths, func(i, j int) bool {
+		return filePathSorter(paths[i]) < filePathSorter(paths[j])
+	})
+}
+
+func filePathSorter(path string) string {
+	var ret string
+	if path[len(path)-1] == filepath.Separator {
+		ret += "1"
+	} else {
+		ret += "0"
+	}
+	if strings.HasPrefix(filepath.Base(path), ".") {
+		ret += "1"
+	} else {
+		ret += "0"
+	}
+	return ret + path
+}
+
+func samePrefix(results []string) string {
+	var xs string
+	for i, ys := range results {
+		if i == 0 {
+			xs = ys
+		} else {
+			yss := []rune(ys)
+			for j, x := range xs {
+				if j < len(yss) {
+					if x != yss[j] {
+						xs = string(yss[:j])
+						break
+					}
+				} else {
+					xs = string(yss)
+					break
+				}
+			}
+		}
+	}
+	return xs
+}
