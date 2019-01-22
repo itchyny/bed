f7b70719903cf5dc09d3fd110d2aa25dd9dac03e
itchyny <itchyny@hatena.ne.jp>
2019-01-22 10:32:18 +0900

    add test case names in tests for searcher

--- searcher/searcher_test.go
+++ searcher/searcher_test.go
@@ -7,6 +7,7 @@ import (
 
 func TestSearcher(t *testing.T) {
 	testCases := []struct {
+		name     string
 		str      string
 		cursor   int64
 		pattern  string
@@ -15,6 +16,7 @@ func TestSearcher(t *testing.T) {
 		err      error
 	}{
 		{
+			name:     "search forward",
 			str:      "abcde",
 			cursor:   0,
 			pattern:  "cd",
@@ -22,6 +24,7 @@ func TestSearcher(t *testing.T) {
 			expected: 2,
 		},
 		{
+			name:    "search forward but not found",
 			str:     "abcde",
 			cursor:  2,
 			pattern: "cd",
@@ -29,6 +32,7 @@ func TestSearcher(t *testing.T) {
 			err:     errNotFound("cd"),
 		},
 		{
+			name:     "search backward",
 			str:      "abcde",
 			cursor:   4,
 			pattern:  "bc",
@@ -36,6 +40,7 @@ func TestSearcher(t *testing.T) {
 			expected: 1,
 		},
 		{
+			name:    "search backward but not found",
 			str:     "abcde",
 			cursor:  0,
 			pattern: "ba",
@@ -43,6 +48,7 @@ func TestSearcher(t *testing.T) {
 			err:     errNotFound("ba"),
 		},
 		{
+			name:     "search large target forward",
 			str:      strings.Repeat(" ", 10*1024*1024+100) + "abcde",
 			cursor:   102,
 			pattern:  "bcd",
@@ -50,6 +56,7 @@ func TestSearcher(t *testing.T) {
 			expected: 10*1024*1024 + 101,
 		},
 		{
+			name:    "search large target forward but not found",
 			str:     strings.Repeat(" ", 10*1024*1024+100) + "abcde",
 			cursor:  102,
 			pattern: "cba",
@@ -57,6 +64,7 @@ func TestSearcher(t *testing.T) {
 			err:     errNotFound("cba"),
 		},
 		{
+			name:     "search large target backward",
 			str:      "abcde" + strings.Repeat(" ", 10*1024*1024),
 			cursor:   10*1024*1024 + 2,
 			pattern:  "bcd",
@@ -64,6 +72,7 @@ func TestSearcher(t *testing.T) {
 			expected: 1,
 		},
 		{
+			name:    "search large target backward but not found",
 			str:     "abcde" + strings.Repeat(" ", 10*1024*1024),
 			cursor:  10*1024*1024 + 2,
 			pattern: "cba",
@@ -72,22 +81,24 @@ func TestSearcher(t *testing.T) {
 		},
 	}
 	for _, testCase := range testCases {
-		s := NewSearcher(strings.NewReader(testCase.str))
-		ch := s.Search(testCase.cursor, testCase.pattern, testCase.forward)
-		select {
-		case x := <-ch:
-			switch x := x.(type) {
-			case error:
-				if testCase.err == nil {
-					t.Error(x)
-				} else if x != testCase.err {
-					t.Errorf("Error should be %v but got %v", testCase.err, x)
-				}
-			case int64:
-				if x != testCase.expected {
-					t.Errorf("Search result should be %d but got %d", testCase.expected, x)
+		t.Run(testCase.name, func(t *testing.T) {
+			s := NewSearcher(strings.NewReader(testCase.str))
+			ch := s.Search(testCase.cursor, testCase.pattern, testCase.forward)
+			select {
+			case x := <-ch:
+				switch x := x.(type) {
+				case error:
+					if testCase.err == nil {
+						t.Error(x)
+					} else if x != testCase.err {
+						t.Errorf("Error should be %v but got %v", testCase.err, x)
+					}
+				case int64:
+					if x != testCase.expected {
+						t.Errorf("Search result should be %d but got %d", testCase.expected, x)
+					}
 				}
 			}
-		}
+		})
 	}
 }
