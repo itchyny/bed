57caa13c81f59021b0db6dc59f18dd172065ccc7
itchyny <itchyny@hatena.ne.jp>
2019-01-22 22:59:46 +0900

    implement search by hex/bin literals

--- searcher/pattern.go
+++ searcher/pattern.go
@@ -1,8 +1,66 @@
 package searcher
 
-import "unicode/utf8"
+import (
+	"errors"
+	"unicode/utf8"
+)
 
-func patternToTarget(pattern []byte) []byte {
+func patternToTarget(pattern []byte) ([]byte, error) {
+	if len(pattern) > 3 && pattern[0] == '0' {
+		switch pattern[1] {
+		case 'x', 'X':
+			return decodeHexLiteral(pattern)
+		case 'b', 'B':
+			return decodeBinLiteral(pattern)
+		}
+	}
+	return unescapePattern(pattern), nil
+}
+
+func decodeHexLiteral(pattern []byte) ([]byte, error) {
+	bs := make([]byte, 0, len(pattern)/2+1)
+	var c byte
+	var lower bool
+	for i := 2; i < len(pattern); i++ {
+		if !isHex(pattern[i]) {
+			return nil, errors.New("invalid hex pattern: " + string(pattern))
+		}
+		c = c<<4 | hexToDigit(pattern[i])
+		if lower {
+			bs = append(bs, c)
+			c = 0
+		}
+		lower = !lower
+	}
+	if lower {
+		bs = append(bs, c<<4)
+	}
+	return bs, nil
+}
+
+func decodeBinLiteral(pattern []byte) ([]byte, error) {
+	bs := make([]byte, 0, len(pattern)/16+1)
+	var c byte
+	var bits int
+	for i := 2; i < len(pattern); i++ {
+		if !isBin(pattern[i]) {
+			return nil, errors.New("invalid bin pattern: " + string(pattern))
+		}
+		c = c<<1 | hexToDigit(pattern[i])
+		bits++
+		if bits == 8 {
+			bits = 0
+			bs = append(bs, c)
+			c = 0
+		}
+	}
+	if bits > 0 {
+		bs = append(bs, c<<uint(8-bits))
+	}
+	return bs, nil
+}
+
+func unescapePattern(pattern []byte) []byte {
 	var escape bool
 	bs := make([]byte, 0, len(pattern))
 	for i := 0; i < len(pattern); i++ {
@@ -90,4 +148,8 @@ func hexToDigit(b byte) byte {
 	default:
 		return 0
 	}
+}
+
+func isBin(b byte) bool {
+	return b == '0' || b == '1'
 }
